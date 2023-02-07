75ccb370fa4f5367434593ffa3c73e907e901c2f
msnkr <mikyle.designs@gmail.com>
2023-02-07 11:34:28 +0200

    .

--- playground.js
+++ playground.js
@@ -12,3 +12,10 @@
 // y = x.getElementsByTagName('p')
 
 // console.log(y);
+
+
+document.getElementById('button-2').onclick = changeRyu;
+
+function changeRyu() {
+    document.getElementById('ryu-1').src = 'ryu-2.jpg'
+};
