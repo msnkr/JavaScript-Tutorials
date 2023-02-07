7e967ed86ffd7a4d14e0e630cb7c6276aa4f2f80
msnkr <mikyle.designs@gmail.com>
2023-02-07 10:47:05 +0200

    .

---
+++ playground.js
@@ -0,0 +1,14 @@
+// document.getElementById('hello-there').innerHTML='HelloWorld!'
+
+
+// document.getElementById('random-button').onclick = function changeThis() {
+//     document.getElementById('change-this').innerHTML = 'I changed this'
+// };
+
+// body = document.documentElement
+// console.log(body);
+
+// x = document.getElementById('main');
+// y = x.getElementsByTagName('p')
+
+// console.log(y);
