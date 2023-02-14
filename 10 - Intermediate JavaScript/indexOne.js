6dbe9f4c6350454b4a2105e14d4f4d06de9747fe
msnkr <mikyle.designs@gmail.com>
2023-02-14 06:50:34 +0200

    .

---
+++ 10 - Intermediate JavaScript/indexOne.js
@@ -0,0 +1,12 @@
+// let name1 = 'mikyle';
+// let name2 = 'kashia';
+
+// let percentage = Math.random();
+// percentage = Math.floor(percentage * 100) + 1;
+// if ( percentage > 70 ) {
+//     console.log(`${name1} and ${name2} score: ${percentage}%. You love eachother like Kanye loves Kanye `);
+// } else if (percentage > 30 && percentage <=70) {
+//     console.log(`${name1} and ${name2} score: ${percentage}%`);
+// } else {
+//     console.log(`${name1} and ${name2} score: ${percentage}%. You go together like oil and water. `);
+// }
