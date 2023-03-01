c817c689f27327740590bdebc7acfcab9edf1ccc
msnkr <mikyle.designs@gmail.com>
2023-03-01 12:31:12 +0200

    .

--- playground.js
+++ playground.js
@@ -13,23 +13,16 @@
 
 // console.log(y);
 
-
-// document.getElementById('button-2').onclick = changeRyu;
-
-// function changeRyu() {
-//     document.getElementById('ryu-2').src = 'ryu-1.jpg'
-//     setInterval(function() {
-//     document.getElementById('ryu-1').src = 'ryu-2.jpg'
+// document.querySelector("#button-2").addEventListener("click", function() {
+//     document.querySelector("#ryu-2").src = "ryu-1.jpg"
+//     setTimeout(function() {
+//     document.querySelector("#ryu-2").src = "ryu-2.jpg"
 //     }, 100)
-// };
+// })
 
-
-document.querySelector("#button-2").addEventListener("click", function() {
-    document.querySelector("#ryu-2").src = "ryu-1.jpg"
-    setTimeout(function() {
-    document.querySelector("#ryu-2").src = "ryu-2.jpg"
-    }, 100)
-})
+// document.addEventListener("keypress", function(event) {
+//     console.log(event.key);
+// })
 
 // elem = document.getElementById('hello-there');
 // elem.innerHTML = 'test';
@@ -52,5 +45,38 @@ document.querySelector("#button-2").addEventListener("click", function() {
 //     document.getElementById('hide-text').style.visibility = 'visible';
 // }
 
-// document.getElementById('animation-start').style.backgroundColor = 'yellow'
+// document.querySelector("#button-2").addEventListener("click", function() {
+//     document.querySelector("#ryu-2").src = "ryu-1.jpg"
+//     setTimeout(function() {
+//     document.querySelector("#ryu-2").src = "ryu-2.jpg"
+//     }, 100)
+// })
 
+
+// elem = document.querySelector("#animation-start");
+
+// elem.addEventListener("click", animateYellow);
+
+// function animateYellow() {
+//    elem.style.backgroundColor = 'yellow';
+//    setTimeout(function() {
+//    elem.style.backgroundColor = '';
+//    }, 100)
+// }
+
+
+elem = document.querySelectorAll("#ryu-2")[1];
+
+document.querySelector("#punch-button-1").addEventListener("click", function() {
+    elem.src = "ryu-1.jpg"
+    setTimeout(function() {
+    elem.src = "ryu-2.jpg"
+    }, 100)
+})
+
+document.querySelector("#punch-button-2").addEventListener("click", function() {
+    elem.src = "ryu-3.jpg"
+    setTimeout(function() {
+    elem.src = "ryu-2.jpg"
+    }, 100)
+})
