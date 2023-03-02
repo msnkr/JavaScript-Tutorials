e4d0858206e8e02f556987c72809265315db7b46
msnkr <mikyle.designs@gmail.com>
2023-03-02 23:07:42 +0200

    .

--- 15 - Boss Challenge Level 2/game.js
+++ 15 - Boss Challenge Level 2/game.js
@@ -5,14 +5,28 @@
 let buttonColors = ["red", "green", "blue", "yellow"];
 let randomChosenColor = buttonColors[randomSequence()];
 let gamePattern = [];
+let addColorToPattern = gamePattern.push(randomChosenColor);
+let randomColorHex = "#" + randomChosenColor;
+let userChosenPattern = [];
 
 // Functions
 function randomSequence() {
     let randomNumber = Math.random() * 4;
     return Math.floor(randomNumber);
-}
+};
 
-let addColorToPattern = gamePattern.push(randomChosenColor);
+function randomColorSelected() {
+    $(randomColorHex).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    var randomSound = new Audio("sounds/" + randomChosenColor + ".mp3");
+    randomSound.play();
+};
 
 
+// Events
+$(randomColorHex).on("click", randomColorSelected);
+$(".btn").on("click", function() {
+    userChosenColor = this.id;
+    userChosenPattern.push(userChosenColor);
+})
 
+console.log(userChosenPattern)
