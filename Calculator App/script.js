e48062e3eb7466b86ffbe8ea0ed63c0cfbaa0234
msnkr <mikyle.designs@gmail.com>
2023-03-17 15:57:55 +0200

    .

--- Calculator App/script.js
+++ Calculator App/script.js
@@ -1,3 +1,10 @@
-let numbers = document.getElementById("number");
+let numbers = document.querySelectorAll("#number")
 let symbol = document.getElementById("symbol");
 let screen = document.getElementById("screen");
+let container = document.getElementById("container")
+
+container.addEventListener("click", e => {
+    if (e.target.classList.contains("number")) {
+        
+    }
+})
