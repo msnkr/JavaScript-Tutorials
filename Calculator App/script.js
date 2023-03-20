f97fb648e140ab9033977b4100b012d20554273d
msnkr <mikyle.designs@gmail.com>
2023-03-20 12:55:25 +0200

    .

--- Calculator App/script.js
+++ Calculator App/script.js
@@ -1,10 +1,24 @@
-let numbers = document.querySelectorAll("#number")
-let symbol = document.getElementById("symbol");
+let container = document.getElementById("container");
+let number =  document.querySelectorAll("#number");
+let symbol = document.querySelectorAll("#symbol");
 let screen = document.getElementById("screen");
-let container = document.getElementById("container")
 
-container.addEventListener("click", e => {
-    if (e.target.classList.contains("number")) {
-        
-    }
-})
+// Functions
+function add(numA, numB) {
+    let sum = numA + numB;
+}
+
+function subtract(numA, numB) {
+    let sum = numA - numB;
+}
+
+function divide(numA, numB) {
+    let sum = numA / numB;
+}
+
+function multiply(numA, numB) {
+    let sum = numA * numB;
+}
+
+
+// Events
