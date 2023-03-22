f1c29c8d7f4745f66f275d0a03a72496e84359a9
msnkr <mikyle.designs@gmail.com>
2023-03-22 14:48:44 +0200

    .

--- Calculator App/script.js
+++ Calculator App/script.js
@@ -2,11 +2,20 @@ let container = document.getElementById("container");
 let number =  document.querySelectorAll("#number");
 let symbol = document.querySelectorAll("#symbol");
 let screen = document.getElementById("screen");
+let firstNum = "";
+let secondNum = "";
+
 
 
 // Functions
-function displayOnScreen(num) {
-    screen.innerHTML = num;
+// Add number to screen
+
+function showToScreen(x) {
+    screen.innerHTML = x;
+}
+
+function addAnimation(x) {
+    x.classList.remove("pressed");
 }
 
 function add(numA, numB) {
@@ -28,10 +37,15 @@ function multiply(numA, numB) {
 // Events
 container.addEventListener("click", e => {
     if (e.target.classList.contains("number")) {
-        let numA = +e.target.textContent;
-        displayOnScreen(numA);
-    } else {
-        let sym = e.target.textContent;
+        btnPressed = e.target;
+        btnPressed.classList.add("pressed")
+        firstNum += btnPressed.textContent;
+        setInterval(addAnimation, 100, btnPressed);
+        showToScreen(+firstNum)
+    } else if (e.target.classList.contains("symbol")) {
+        symPressed = e.target;
+        symPressed.classList.add("pressed")
+        setInterval(addAnimation, 100, symPressed);
+        screen.innerHTML = "";
     }
-
 })
