a83fff04918677a85ca20173e080cac66dde12e5
msnkr <mikyle.designs@gmail.com>
2023-03-28 15:55:52 +0200

    .

--- Weather app/script.js
+++ Weather app/script.js
@@ -7,13 +7,16 @@ function getWeatherData() {
     fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m")
     .then(res => res.json())
     .then(data => {
+
         let temperature = data.hourly.temperature_2m;
         let time = data.hourly.time;
 
-        temperature.forEach(element => {
-            weatherCelcius.innerHTML = `<div>${element}</div`
+        let output = "";
+        temperature.forEach((element, index) => {
+            let t = time[index]
+            output += `<div><p> ${element} ${t} </p></div>`
         });
-        time.forEach(element => console.log(element));
+        weatherCelcius.innerHTML = output
     });
 }
 
--- moreScraping/index.js
+++ moreScraping/index.js
@@ -1,25 +1,19 @@
 const puppeteer = require("puppeteer");
 
-// function highKarma() {
-
-// }
-
 async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto("https://news.ycombinator.com/");
-    const titles = await page.evaluate( () => Array.from( document.querySelectorAll(".athing"), (e) => ({
-        title: e.querySelector(".titleline a").textContent,
-        link: e.querySelector(".titleline a").href,
+    await page.goto("https://freemeteo.co.za/weather/johannesburg/7-days/list/?gid=993800&language=english");
 
-    }) ));
+    // const weather = await page.evaluate(() => Array.from(document.querySelectorAll("tr"), (e) => ({
+    //     date: e.querySelector(".small").textContent,
 
-    const votes = await page.evaluate( () => Array.from(document.querySelectorAll(".subline"), (e) => ({
-        vote: e.querySelector(".score").textContent,
-    }) ))
+    // })))
 
-    console.log(titles)
-    console.log(votes);
+    // console.log(weather);
+
+    const title = await page.content();
+    console.log(title);
 
     await browser.close();
 };
