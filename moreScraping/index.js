98d63a12124c536fedf12a0dd0308ec79f3edcb9
msnkr <mikyle.designs@gmail.com>
2023-03-29 15:32:46 +0200

    Update index.js

--- moreScraping/index.js
+++ moreScraping/index.js
@@ -3,17 +3,19 @@ const puppeteer = require("puppeteer");
 async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto("https://www.theweathernetwork.com/za/weather/gauteng/johannesburg");
 
-    // const weather = await page.evaluate(() => Array.from(document.querySelectorAll("tr"), (e) => ({
-    //     date: e.querySelector(".small").textContent,
+    page.setDefaultTimeout(50000)
+    await page.goto("https://github.com/msnkr/friendly-system");
 
+    // posts = await page.evaluate(() => Array.from( document.querySelectorAll(".titleline"), (e) => ({
+
+    //     title: e.querySelector("a").textContent,
+    //     link: e.querySelector("a").href,
     // })))
 
-    // console.log(weather);
-
-    const title = await page.evaluate(document.title);
-    console.log(title);
+    // console.log(posts);
+    page.setDefaultTimeout(50000)
+    await page.screenshot({ path: "blueRice.png", fullPage: true })
 
     await browser.close();
 };
