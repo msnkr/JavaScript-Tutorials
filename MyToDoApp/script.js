4470a4e124a0d7928e91d3561b66db1941a3cbbe
msnkr <mikyle.designs@gmail.com>
2023-04-06 11:28:14 +0200

    .

--- MyToDoApp/script.js
+++ MyToDoApp/script.js
@@ -1,26 +1,31 @@
 let textInput = document.getElementById("text-input");
 let submitBtn = document.getElementById("submit-btn");
 let listItems = document.getElementById("items");
-// let cancelItem = document.getElementById("cancel");
+let bodyCancelBtn = document.body;
+
 
 // Functions
 //Add to list
 function addToList(toDo) {
-    listItems.innerHTML += `<li>${toDo}</li>`
+    if (toDo !== "") {
+        listItems.innerHTML += `<li> ${toDo} <button class="cancel-btn"><i class="fa fa-times"></i></button> </li>`;
+    }
 }
 // remove from list
-function removeFromList(toDo) {
-    this.innerHTML = "";
-}
-// Clean the screen
+// function removeListItem(e) {
+//    console.log(e);
+// }
 // Add to local storage
 
 // Events
 //get input
-submitBtn.addEventListener("click", e => {
+submitBtn.addEventListener("click", () => {
    addToList(textInput.value);
+   textInput.value = "";
 })
 // remove input
-// cancelItem.addEventListener("click", e => {
-//     removeFromList(e.target)
-// })
+bodyCancelBtn.addEventListener("click", e => {
+    if (e.target.classList.contains("fa-times")) {
+        console.log(e)
+    }
+})
