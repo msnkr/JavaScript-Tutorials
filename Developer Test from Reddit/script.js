62cacb446247e3bc457bbfa52e2be3f50aeda713
msnkr <mikyle.designs@gmail.com>
2023-04-09 22:48:19 +0200

    .

--- Developer Test from Reddit/script.js
+++ Developer Test from Reddit/script.js
@@ -6,13 +6,34 @@
 // Grab the submit button
 // Grab the small tag
 
-var randomUsername = "john"
-var ramdomPassword = "mrSmith"
+// create a function that takes the username and password value and checks them against the "random" ones
 
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
+let randomUsername = "john"
+let ramdomPassword = "mrSmith"
+
+let username = document.getElementById("username").value;
+let password = document.getElementById("password");
 let submitBtn = document.getElementById("submit-btn");
-let smallTag = document.getElementById("small-tag");
+let smallTag = document.querySelector("small")
 
 
 
+
+function checkCreds() {
+    if (username !== randomUsername || username == "" || password.value !== ramdomPassword || password.value == "") {
+        smallTag.classList.remove("hide")
+        smallTag.classList.add("incorrect")
+        location.reload();
+    } else {
+        smallTag.classList.remove("hide")
+        smallTag.classList.add("correct")
+        smallTag.innerHTML = "Correct";
+    }
+}
+
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    checkCreds()
+})
