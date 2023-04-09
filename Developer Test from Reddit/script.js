751d2baa9e3a4bb188b6c2e05d664aa6101668ed
msnkr <mikyle.designs@gmail.com>
2023-04-09 19:59:43 +0200

    .

--- Developer Test from Reddit/script.js
+++ Developer Test from Reddit/script.js
@@ -1,9 +1,19 @@
+// Match username and value with var
+// If dont match, show error
+// Else show login successful
+
+// Grab username and password inputs
+// Grab the submit button
+// Grab the small tag
 
 var randomUsername = "john"
 var ramdomPassword = "mrSmith"
 
-var username = document.getElementById("username");
-var password = document.getElementById("password");
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+let submitBtn = document.getElementById("submit-btn");
+let smallTag = document.getElementById("small-tag");
 
+submitBtn.addEventListener("click", () => console.log("hello"))
 
 
