3740bbaf47a852b8757db27706d8ef2f28d536fc
msnkr <mikyle.designs@gmail.com>
2023-04-11 16:12:24 +0200

    .

--- Developer Test from Reddit/script.js
+++ Developer Test from Reddit/script.js
@@ -1,3 +1,5 @@
-// Make landing page.
-// On sign up button, form popup
-// FOrm must block out other items on page and display in the middle
+
+let signUpBtn = document.getElementById("sign-up");
+let formMain = document.getElementById("form-main");
+
+signUpBtn.addEventListener("click", () => formMain.classList.remove("hidden"))
