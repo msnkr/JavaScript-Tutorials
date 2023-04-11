4ced5095c234445b6f7de3d0a83207770d397a2e
msnkr <mikyle.designs@gmail.com>
2023-04-11 19:27:12 +0200

    .

--- Developer Test from Reddit/script.js
+++
@@ -1,5 +0,0 @@
-
-let signUpBtn = document.getElementById("sign-up");
-let formMain = document.getElementById("form-main");
-
-signUpBtn.addEventListener("click", () => formMain.classList.remove("hidden"))
