3a846cfcacc075aa86e27ee4dc8c597f5b6528eb
msnkr <mikyle.designs@gmail.com>
2023-04-19 13:41:42 +0200

    .

--- Playground/Pokedex/script.js
+++ Playground/Pokedex/script.js
@@ -3,9 +3,23 @@
 let pokemonName = document.getElementById("pokemon-title");
 let pokemonImage = document.getElementById("pokemon-image");
 let pokemonType = document.getElementById("pokemon-type");
+let container = document.getElementById("pokemon-container")
 let pokemonData = [];
 
+// Update the dom
+function updateDom(image, name, type) {
+    documentTitle = document.createElement("h3");
+    documentTitle.innerHTML = name;
+    container.appendChild(documentTitle)
 
+    documentImage = document.createElement("img");
+    documentImage.src = image;
+    container.appendChild(documentImage)
+
+    documentType = document.createElement("h4");
+    documentType.innerHTML = type;
+    container.appendChild(documentType)
+}
 
 // Get one single pokemon and add to the dom
 function getSinglePokemon(data) {
@@ -13,13 +27,10 @@ function getSinglePokemon(data) {
         let image = element.sprites.front_default;
         let name = element.species.name;
         let type = element.types[0].type["name"];
-    
-        pokemonName.innerHTML = name;
-        pokemonImage.src = image;
-        pokemonType.innerHTML = type;
+
+        updateDom(image, name, type)
     })
 }
-
 
 // Get the List of pokemon
 async function getPokemonList() {
