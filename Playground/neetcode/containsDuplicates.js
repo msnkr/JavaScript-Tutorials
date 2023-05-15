2fc333deca798ce3887b580884e80b4351309a1f
msnkr <mikyle.designs@gmail.com>
2023-05-15 15:55:56 +0200

    Create containsDuplicates.js

---
+++ Playground/neetcode/containsDuplicates.js
@@ -0,0 +1,8 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+const nums = [1, 2, 3, 1];
+
+const checkNumbers = nums.map( num => {
+    return num ;
+} )
+
+console.log(checkNumbers);
