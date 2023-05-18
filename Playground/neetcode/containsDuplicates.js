87d881459e7773acb6e9a9ba9bff5297860bc12c
msnkr <74895279+msnkr@users.noreply.github.com>
2023-05-18 12:19:17 +0200

    Delete Playground/neetcode directory

--- Playground/neetcode/containsDuplicates.js
+++
@@ -1,6 +0,0 @@
-// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-const nums = [1, 2, 3, 4];
-// const nums = [1, 2, 3, 1];
-
-// Loop through all numbers
-// 
