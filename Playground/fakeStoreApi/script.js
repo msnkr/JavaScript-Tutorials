287b16e26d2fc62f24a72b54e9a5121ecc6665cf
msnkr <mikyle.designs@gmail.com>
2023-05-23 16:24:20 +0200

    .

--- Playground/fakeStoreApi/script.js
+++ Playground/fakeStoreApi/script.js
@@ -1,5 +1,23 @@
-// Call items based on category
+const electronicsHome = document.querySelector(".grid");
 
-fetch('https://fakestoreapi.com/products')
+
+
+function showElectronicsHome(item) {
+    let output = document.createElement("div");
+    output.innerHTML = `<div class="grid-item">
+    ${item.title}
+    <div class="product-image" style="background: url(${item.image}) no-repeat center center/cover"></div>
+    </div>`;
+    electronicsHome.appendChild(output);
+}
+
+fetch('https://fakestoreapi.com/products/category/electronics')
             .then(res=>res.json())
-            .then(json=>console.log(json))
+            .then(json=>{
+                for (let index = 0; index < json.length; index++) {
+                    if (index < 4) {
+                        showElectronicsHome(json[index]);
+                    }
+                }
+            });
+
