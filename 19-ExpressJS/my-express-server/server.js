1e07f942fcf09809ee0b6744fd1763c7c808d587
msnkr <mikyle.designs@gmail.com>
2023-05-23 23:11:46 +0200

    .

--- 19-ExpressJS/my-express-server/server.js
+++ 19-ExpressJS/my-express-server/server.js
@@ -9,5 +9,6 @@ app.get("/contact", (request, response) => response.send("<p>Contact Me</p>"));
 
 app.get("/about", (request, response) => response.send("<p>About Me 'LOREM10000' Lol</p>"));
 
+app.get("/cv", (req, res) => res.send("<h1>This is where I would put my cv</h1>"))
 
 app.listen(port, () => console.log(`Server started on port: ${port }`));
