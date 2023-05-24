6f3e684d5a5607a5114f7433ec3b91a34e41f2a7
msnkr <mikyle.designs@gmail.com>
2023-05-24 11:55:04 +0200

    .

--- Playground/fakeStoreApi/script.js
+++ Playground/fakeStoreApi/script.js
@@ -1,23 +1,28 @@
-const electronicsHome = document.querySelector(".grid");
+const grid = document.querySelector(".grid");
 
 
-
-function showElectronicsHome(item) {
+function showHome(item) {
     let output = document.createElement("div");
     output.innerHTML = `<div class="grid-item">
-    ${item.title}</div>
-    <div class="product-image" style="background: url(${item.image}) no-repeat center center/cover"></div>
-    `;
-    electronicsHome.appendChild(output);
+    ${item.title}
+    <div class="product-image" style="background: url(${item.image}) no-repeat center center/200px 200px"></div></div>`;
+    grid.appendChild(output);
 }
 
-fetch('https://fakestoreapi.com/products/category/electronics')
-            .then(res=>res.json())
-            .then(json=>{
-                for (let index = 0; index < json.length; index++) {
-                    if (index < 4) {
-                        showElectronicsHome(json[index]);
-                    }
-                }
-            });
+function fetchItems(what) {
+fetch(`https://fakestoreapi.com/products/category/${what}`)
+    .then(res=>res.json())
+    .then(json=>{
+        for (let index = 0; index < json.length; index++) {
+            showHome(json[index])
+        }
+    });
+}
 
+fetch('https://fakestoreapi.com/products/categories')
+            .then(res=>res.json())
+            .then(json=> {
+                json.forEach(element => {
+                    fetchItems(element);
+                });
+            })
