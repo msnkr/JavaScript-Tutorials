1f375ca86d5a19c2d007c49ab718679d24d85ec7
msnkr <mikyle.designs@gmail.com>
2023-05-25 12:02:32 +0200

    .

--- 19-ExpressJS/my-express-server/templateEngineTest/templateEngineTest.js
+++ 19-ExpressJS/my-express-server/templateEngineTest/templateEngineTest.js
@@ -1,23 +1,21 @@
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
+const path = require("path")
 
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
+app.use(express.static("static"));
 
 hbs.registerPartials(__dirname + "/views/partials");
 
 
 app.get("/", (req, res) => {
-    const name = "Mikyle";
-    const age = 32;
-    res.render("index", { name, age });
+    res.render("index");
 })
 
 app.get("/contact", (req, res) => {
-    const name = "Mikyle";
-    const number = "445-345-3432";
-    res.render("contact", {name, number});
+    res.render("contact");
 })
 
 
