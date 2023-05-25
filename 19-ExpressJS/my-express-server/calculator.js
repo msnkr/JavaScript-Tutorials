7b9c15efa51b9caf6be735295c190d4d3eecfb1e
msnkr <mikyle.designs@gmail.com>
2023-05-25 22:27:03 +0200

    .

--- 19-ExpressJS/my-express-server/calculator.js
+++ 19-ExpressJS/my-express-server/calculator.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-const port = 3000;
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.get("/", (req, res) => res.send("Hello"))
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"))
+app.post("/", (req, res) => {
+    var num1 = req.body.num1;
+    var num2 = req.body.num2;
+
+    var result = Number(num1) + Number(num2);
+    res.send("The answer is: "+result)
+})
 
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(3000, () => console.log(`http://localhost:3000`));
