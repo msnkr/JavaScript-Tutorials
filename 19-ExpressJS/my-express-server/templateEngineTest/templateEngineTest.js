f8f4cfe5d6d0da3286a7069a353b8cc0a08d9960
msnkr <mikyle.designs@gmail.com>
2023-05-25 11:02:42 +0200

    .

--- 19-ExpressJS/my-express-server/templateEngineTest/templateEngineTest.js
+++ 19-ExpressJS/my-express-server/templateEngineTest/templateEngineTest.js
@@ -1,9 +1,11 @@
 const express = require("express");
-
+const hbs = require("hbs");
 const app = express();
 
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
+
+hbs.registerPartials(__dirname + "/views/partials");
 
 
 app.get("/", (req, res) => {
