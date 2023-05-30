d6bc070b6ea2b9c0eb9b5267fe1326fc7db6690f
msnkr <mikyle.designs@gmail.com>
2023-05-30 12:50:18 +0200

    .

--- ReactJSProject/react-tutorial/src/App.js
+++ ReactJSProject/react-tutorial/src/App.js
@@ -1,9 +1,31 @@
 import react from "react";
+import './App.css';
 
-function MyButton() {
-  return (
-    <button>Submit</button>
-  )
+// function MyButton() {
+//   return (
+//     <button>Submit</button>
+//   )
+// }
+
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Welcome to my App</h1>
+//       <MyButton />
+//     </div>
+//   );
+// }
+
+const user = {
+  name: "Mikyle",
+  age: 32,
 }
 
-export default MyButton;
+export default function Profile() {
+  return (
+    <div>
+      <h1 className="name">{user.name}</h1>
+      <h1>{user.age}</h1>
+    </div>
+  )
+}
