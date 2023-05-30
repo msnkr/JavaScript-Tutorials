31dc505e59011db9e03cdc0bbadac537b003dd8d
msnkr <mikyle.designs@gmail.com>
2023-05-30 08:24:24 +0200

    .

--- 19-ExpressJS/my-express-server/Another Test/server.js
+++ 19-ExpressJS/my-express-server/Another Test/server.js
@@ -8,8 +8,16 @@ app.use(express.static("public"))
 
 hbs.registerPartials(__dirname + "/views/partials")
 
+let demo = {
+    name: "Mikyle",
+    age: 32,
+    traits: ["Drk Hair", "Blue Eyes", "Tall", "Muscular", "Tattoos"]
+}
+
+
+
 app.get("/", (req, res) => {
-    res.render("index.hbs")
+    res.render("index.hbs", {demo: demo});
 })
 
 app.listen(3000, () => console.log("Listening ==> http://localhost:3000"));
