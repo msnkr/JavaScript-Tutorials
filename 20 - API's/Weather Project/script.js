0edbc94d1e37f95b5d66ec1860e50003f79c3acb
msnkr <mikyle.designs@gmail.com>
2023-06-07 21:34:13 +0200

    m

--- 20 - API's/Weather Project/script.js
+++ 20 - API's/Weather Project/script.js
@@ -6,17 +6,13 @@ const app = express()
 app.get("/", (req, res) => {
     
     https.get("https://api.openweathermap.org/data/2.5/weather?q=johannesburg&units=metric&appid=888fc6acf8d5a7a7b9e51209de4178cc ", response => {
-        response.on("data", data => {
-            let weatherData = JSON.parse(data);
-            let weather = {
-                temp: weatherData.main.temp,
-                description: weatherData.weather[0].main,
-                icon: weatherData.weather[0].icon,
-            }
-            console.log(weather);
+        response.on(("data"), data => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+
+            res.send(temp)
         })
     })
-    res.send("hello")
 })
 
 app.listen(3000, () => console.log("Running"))
