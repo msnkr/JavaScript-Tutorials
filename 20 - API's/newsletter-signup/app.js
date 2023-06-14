6bd8b076fc068a8b42d5ccd5bfdcc5a476296cda
msnkr <mikyle.designs@gmail.com>
2023-06-14 22:18:37 +0200

    .

--- 20 - API's/newsletter-signup/app.js
+++ 20 - API's/newsletter-signup/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 
@@ -19,19 +20,42 @@ app.post("/", (req, res) => {
     const lastName = split[1];
     
     let data = {
-        members: {
+        members: [
+            {
             email_address: email,
             status: "subscribed",
             merge_fields: {
                 FNAME: firstName,
                 LNAME: lastName,
+                }
             }
-        }
+        ]
     }
 
     let jsonData = JSON.stringify(data);
-    res.sendFile(__dirname + "/success.html")
+    const options = {
+        method: "POST",
+        auth: `<Mikyle>:${key}`,
+    }
+
+    const request = https.request(url, options, response => {
+        if (response.statusCode == 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failire.html");
+        }
+        
+        response.on("data", data => {
+            console.log(JSON.parse(data));
+        })
+    })
+    
+    request.write(jsonData)
+    request.end();
 })
 
 
 app.listen(3000, () => console.log("http://localhost:3000"))
+
+const url =  "https://us21.api.mailchimp.com/3.0/lists/34b79bbef3"
+const key = "c204bfa6ac78d24f1f4a6bd25290651e-us21"
