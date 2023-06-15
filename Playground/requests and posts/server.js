b328b179b3b35215f829a3996e41f9e2fe39f387
msnkr <mikyle.designs@gmail.com>
2023-06-15 15:59:06 +0200

    .

--- Playground/requests and posts/server.js
+++ Playground/requests and posts/server.js
@@ -5,12 +5,22 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"))
 
-app.get("/", (req, res) => {
-    axios.get("https://jsonplaceholder.typicode.com/posts/20")
-    .then((response) => {
-        console.log(response.data)
+app.get("/", (request, response) => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => {
+        const data = res.data;
+
+        const jsonData = {};
+        data.forEach(item => {
+            jsonData = {
+                title: item.title,
+                body: item.body,
+            }
+        console.log(jsonData)
+        });
+
+        response.send();
     })
-    res.sendFile(__dirname + "/new-post.html");
 })
 
 app.listen(3000, () => console.log("Running"));
