3ab8b41780efdfde6ec304c06a64ea4784f90c74
msnkr <mikyle.designs@gmail.com>
2023-06-16 21:58:36 +0200

    .

--- 21 - To Do List/server.js
+++ 21 - To Do List/server.js
@@ -4,7 +4,14 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello")
+    let date = new Date();
+    let currentDay = date.getDay();
+
+    if (currentDay == 6 || currentDay == 0) {
+        res.write("<h1>Yay. Its the weekend</h1>");
+    } else {
+        res.sendFile(__dirname + "/index.html")
+    }
 })
 
 app.listen(3000, () => console.log("Running"))
