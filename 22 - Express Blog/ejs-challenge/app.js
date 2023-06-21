17904ccceaa978684d14d9eacd826e6665ed5759
msnkr <mikyle.designs@gmail.com>
2023-06-21 23:14:55 +0200

    .

--- 22 - Express Blog/ejs-challenge/app.js
+++ 22 - Express Blog/ejs-challenge/app.js
@@ -1,56 +1,74 @@
-
 const express = require("express");
 const bodyParser = require("body-parser");
+var _ = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+var array = require("lodash/array");
+
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 let posts = [];
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 
 // Home Page
 app.get("/", (req, res) => {
-  res.render("home", {homeContent: homeStartingContent, posts:posts});
-})
+  res.render("home", { homeContent: homeStartingContent, posts: posts });
+});
 
 // About Page
 app.get("/about", (req, res) => {
-  res.render("about", {aboutContent: aboutContent});
-})
+  res.render("about", { aboutContent: aboutContent });
+});
 
 // Contact Us Page
 app.get("/contact", (req, res) => {
-  res.render("contact", {contactContent: contactContent});
-})
+  res.render("contact", { contactContent: contactContent });
+});
 
 // Compose Page
 app.get("/compose", (req, res) => {
-  res.render("compose")
-})
+  res.render("compose");
+});
 
+// Compose Post
 app.post("/compose", (req, res) => {
-
- var newPosts = {
+  var newPosts = {
     title: req.body.title,
-    body: truncateString(req.body.body),
-  }
+    body: req.body.body,
+  };
 
-  posts.push(newPosts)
-  res.redirect("/")
-})
+  posts.push(newPosts);
+  res.redirect("/");
+});
 
-app.listen(3000, function() {
+app.get("/posts/:postId", (req, res) => {
+  const requestedTitle = _.lowerCase(req.params.postId);
+
+  posts.forEach((post) => {
+    const storedTitle = _.lowerCase(post.title);
+
+    if (requestedTitle === storedTitle) {
+      res.render("post", { post: post });
+    } else {
+      res.send("Not found");
+    }
+  });
+});
+
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
 
-
+// This will give a truncate feauture
 function truncateString(originalString) {
   if (originalString.length > 100) {
     return originalString.substring(0, 100) + " ...";
