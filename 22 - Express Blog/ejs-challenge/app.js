72a6bc26c9c3a005f57edeea495bafd4994011ef
msnkr <mikyle.designs@gmail.com>
2023-06-21 09:49:34 +0200

    .

--- 22 - Express Blog/ejs-challenge/app.js
+++ 22 - Express Blog/ejs-challenge/app.js
@@ -7,7 +7,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-let posts = {};
+let posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -37,10 +37,12 @@ app.get("/compose", (req, res) => {
 
 app.post("/compose", (req, res) => {
 
- posts = {
+ var newPosts = {
     title: req.body.title,
     body: req.body.body,
   }
+
+  posts.push(newPosts)
   res.redirect("/")
 })
 
