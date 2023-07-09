1560a4819f8d7a01f140f35d9569e80ee8f30709
msnkr <mikyle.designs@gmail.com>
2023-07-09 20:00:31 +0200

    ,

--- Playground/Website Projects/Adventurer's Haven/server.js
+++ Playground/Website Projects/Adventurer's Haven/server.js
@@ -5,12 +5,36 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+const adventures = [
+  "Hot Air Balloon Ride",
+  "Cave Exploration",
+  "Paragliding",
+  "Dog Sledding",
+  "Desert Safari",
+  "Whitewater Rafting",
+  "Zip Lining",
+  "Volcano Trekking",
+  "Wildlife Safari",
+  "Underwater Exploration",
+  "Bungee Jumping",
+  "Glacier Hiking",
+  "Skydiving",
+  "Rock Climbing",
+  "Deep-sea Fishing",
+  "Sandboarding",
+  "Canyoning",
+  "Treehouse Camping",
+  "Motorbike Tour",
+  "Arctic Expedition"
+];
+
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.get("/calculator", (req, res) => {
-  res.render("calculator");
+  res.render("calculator", {adventures:adventures});
 });
 
 app.listen(3000, () => console.log("running"));
