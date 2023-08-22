c2da88d7af103afc2b4e51e8efddf1fa4181f069
msnkr <mikyle.designs@gmail.com>
2023-08-22 22:18:49 +0200

    .

--- 26 - Mongoose/app.js
+++ 26 - Mongoose/app.js
@@ -1,48 +1,68 @@
 // getting-started.js
+// const mongoose = require("mongoose");
+
+// main().catch((err) => console.log(err));
+
+// async function main() {
+//   await mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+//   const kittySchema = new mongoose.Schema({
+//     name: String,
+//   });
+
+//   kittySchema.methods.speak = function speak() {
+//     const greeting = this.name
+//       ? "Meow name is " + this.name
+//       : "I don't have a name";
+//     console.log(greeting);
+//   };
+
+//   const Kitten = mongoose.model("Kitten", kittySchema);
+
+//   const silence = new Kitten({ name: "Silence" });
+//   const fluffy = new Kitten({ name: "Fluffy" });
+
+//   fluffy.speak();
+//   silence.speak();
+
+//   const kittens = await Kitten.find();
+//   console.log(kittens);
+//
+//   const userSchema = new mongoose.Schema({
+//     name: String,
+//     age: Number,
+//     address: String,
+//     married: Boolean,
+//   });
+
+//   const User = mongoose.model("User", userSchema);
+
+//   const newUser = new User({
+//     name: "John",
+//     age: 32,
+//     address: "32 Sunset Blvd",
+//     married: false,
+//   });
+
+//   console.log(newUser);
+// }
+
 const mongoose = require("mongoose");
 
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  await mongoose.connect("mongodb://127.0.0.1:27017/person");
 
-  const kittySchema = new mongoose.Schema({
+  const personSchema = new mongoose.Schema({
     name: String,
+    age: Number,
   });
 
-  kittySchema.methods.speak = function speak() {
-    const greeting = this.name
-      ? "Meow name is " + this.name
-      : "I don't have a name";
-    console.log(greeting);
-  };
+  const Person = mongoose.model("Person", personSchema);
+  const john = new Person({ name: "john", age: 37 });
+  const peter = new Person({ name: "peter", age: 27 });
+  const angela = new Person({ name: "angela", age: 32 });
 
-  const Kitten = mongoose.model("Kitten", kittySchema);
-
-  const silence = new Kitten({ name: "Silence" });
-  const fluffy = new Kitten({ name: "Fluffy" });
-
-  fluffy.speak();
-  silence.speak();
-
-  //   const kittens = await Kitten.find();
-  //   console.log(kittens);
-  //
-  //   const userSchema = new mongoose.Schema({
-  //     name: String,
-  //     age: Number,
-  //     address: String,
-  //     married: Boolean,
-  //   });
-
-  //   const User = mongoose.model("User", userSchema);
-
-  //   const newUser = new User({
-  //     name: "John",
-  //     age: 32,
-  //     address: "32 Sunset Blvd",
-  //     married: false,
-  //   });
-
-  //   console.log(newUser);
+  Person.insertMany([john, peter, angela]);
 }
